965749642286992217c4c830270c07a7af949bff
Fazil <faziltirtana@tutanota.com>
2023-09-01 23:01:46 +0700

    [ADD] Navigation, Socmed, and Contact Us Footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,142 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import { Footer } from "@mantine/core";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main className="h-[40rem] bg-black"></main>
+      <Footer className="bg-black flex flex-col md:flex-row md:space-x-10 space-y-16 md:space-y-0 px-2 py-14 border-none">
+        <div className="grow flex">
+          <div className="pr-4 basis-1/2">
+            <div className="grid place-content-center h-full p-2">
+              <Image src="/logo.png" width={240} height={240} alt="Logo" />
+            </div>
+          </div>
+          <div className="pl-4 basis-1/2">
+            <div className="flex flex-col space-y-10 h-full justify-center text-white font-semibold">
+              <Link
+                href={"/"}
+                className="hover:text-aqua-2 transition-colors duration-300 ease-out"
+              >
+                Home
+              </Link>
+              <Link
+                href={"/about"}
+                className="hover:text-aqua-2 transition-colors duration-300 ease-out"
+              >
+                About Fasilkom
+              </Link>
+              <Link
+                href={"/registration"}
+                className="hover:text-aqua-2 transition-colors duration-300 ease-out"
+              >
+                Registration Page
+              </Link>
+              <Link
+                href={"/walloffame"}
+                className="hover:text-aqua-2 transition-colors duration-300 ease-out"
+              >
+                Wall of Fame
+              </Link>
+              <Link
+                href={"/merchandise"}
+                className="hover:text-aqua-2 transition-colors duration-300 ease-out"
+              >
+                Merchandise
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <div className="grow flex justify-center">
+          <div className="max-w-[200px]">
+            <div>
+              <div className="flex justify-center md:justify-start mb-6">
+                <h4 className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-screaminGreen-2 to-grass-2">
+                  Find Us on Social Media!
+                </h4>
+              </div>
+              <div className="flex justify-between space-x-5">
+                <div>
+                  <Link
+                    href={"https://www.instagram.com/ohfasilkom"}
+                    className="hover:text-aqua-2 transition-colors duration-300 ease-out"
+                  >
+                    <Image
+                      src="/icon/solid/instagram.png"
+                      width={24}
+                      height={24}
+                      alt="Instagram"
+                    />
+                  </Link>
+                </div>
+                <div>
+                  <Link
+                    href={"https://twitter.com/OHFasilkom"}
+                    className="hover:text-aqua-2 transition-colors duration-300 ease-out"
+                  >
+                    <Image
+                      src="/icon/solid/twitter.png"
+                      width={24}
+                      height={24}
+                      alt="Twitter"
+                    />
+                  </Link>
+                </div>
+                <div>
+                  <Link
+                    href={"https://www.youtube.com/@FasilkomUIOfficial"}
+                    className="hover:text-aqua-2 transition-colors duration-300 ease-out"
+                  >
+                    <Image
+                      src="/icon/solid/youtube.png"
+                      width={24}
+                      height={24}
+                      alt="Youtube"
+                    />
+                  </Link>
+                </div>
+                <div>
+                  <Link
+                    href={"https://www.tiktok.com/@openhousefasilkomui"}
+                    className="hover:text-aqua-2 transition-colors duration-300 ease-out"
+                  >
+                    <Image
+                      src="/icon/solid/tiktok.png"
+                      width={24}
+                      height={24}
+                      alt="Tiktok"
+                    />
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="mt-8">
+              <div className="flex justify-center md:justify-start mb-5">
+                <h4 className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-screaminGreen-2 to-grass-2">
+                  Contact us
+                </h4>
+              </div>
+              <div className="flex justify-between space-x-5">
+                <Link
+                  href={"/"}
+                  className="hover:text-aqua-2 transition-colors duration-300 ease-out flex text-white"
+                >
+                  openhouse@cs.ui.ac.id
+                  <span className="ml-2 grid place-content-center pt-1">
+                    <Image
+                      src="/icon/solid/clipboard.png"
+                      width={15}
+                      height={15}
+                      alt="Tiktok"
+                    ></Image>
+                  </span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Footer>
+    </>
+  );
 }
