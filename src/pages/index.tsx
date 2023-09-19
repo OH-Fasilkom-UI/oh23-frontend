5fa1b2d0c844840017de82a48457a02b40bf1442
Maurice Yang <mauriceyangmy28@gmail.com>
2023-09-19 10:18:08 +0700

    feat: Finishing landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,30 +2,40 @@ import Image from "next/image";
 import { Orbitron } from "next/font/google";
 import { Button } from "@/components/elements/Button";
 import { Countdown } from "@/components/elements/Countdown";
+import { HiBookOpen, HiChevronDoubleRight } from "react-icons/hi2";
 
 const orbitron = Orbitron({ weight: "800", subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className="bg-black">
-    <section className="min-h-screen bg-black flex items-center justify-center relative">
-      <Image src="/footer-bg.png" alt="" fill className="object-cover brightness-[.6]" />
-      <Image src="/block.svg" width={300} height={300} alt="Logo" className=" brightness-[.6] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
-      <div className="z-10 text-center flex flex-col items-center justify-center gap-5">
-        <h1 style={orbitron.style} className="mt-10 text-transparent bg-gradient-to-br from-screaminGreen-4 via-screaminGreen-2 to-lime-3 bg-clip-text font-bold text-4xl md:text-8xl drop-shadow-sm shadow-white">
-          OPEN HOUSE
-          <br />
-          FASILKOM 2023
-        </h1>
-        <Button variant="primary" className="font-satoshi font-bold text-lg md:text-xl">
-          Daftar Sekarang
-        </Button>
-      </div>
-    </section>
-    <section className="py-10 flex flex-col items-center justify-center">
-    <Countdown date={new Date("2023-09-07T06:39:00+0700")}/>
+      <section className="min-h-screen bg-black flex items-center justify-center relative">
+        <Image src="/background.svg" alt="" width={1440} height={900} className="absolute top-0 left-0 object-cover brightness-[.6]" />
+        <div className="z-10 text-center flex flex-col items-center justify-center gap-7 mx-5 backdrop-blur bg-black-3 px-16 py-10 rounded-md">
+          <h1 className="font-satoshi text-white bg-clip-text font-extrabold text-5xl drop-shadow-sm shadow-white">
+            OPEN HOUSE
+            <br />
+            FASILKOM 2023
+          </h1>
+          <div className="font-satoshi text-center text-lg w-full">
+            <h2 className="text-2xl mb-2">Grand Opening</h2>
+            <Countdown date={new Date("2023-09-19T06:39:00+0700")} />
+          </div>
+          <div className="flex items-center gap-3">
+            <Button variant="primary" className="font-satoshi font-bold">
+              <HiChevronDoubleRight />
+              Register Now
+            </Button>
+            <Button variant="secondary" className="font-satoshi font-bold">
+              <HiBookOpen />
+              Learn More
+            </Button>
+          </div>
+        </div>
+      </section>
+      <section className="py-10 flex flex-col items-center justify-center min-h-screen">
 
-    </section>
+      </section>
     </main>
 
   );
