8f8819a3315b41210405133d103d991683caf5b3
secona <77039267+secona@users.noreply.github.com>
2023-10-03 22:55:30 +0700

    feat: add image and props to Prestasi

--- src/components/ListPrestasi.tsx
+++ src/components/ListPrestasi.tsx
@@ -1,8 +1,17 @@
-const Prestasi = () => {
+import Image from 'next/image';
+
+interface PrestasiProps {
+  imgSrc: string;
+  title: string;
+  description: string
+}
+
+const Prestasi = (props: PrestasiProps) => {
   return (
-    <div className='border-gray-100 rounded-md border-2 border-opacity-10 backdrop-blur py-9 px-10 w-[15rem]'>
-      <h2 className="text-xl text-white-1 mb-4">Heading</h2>
-      <p className='text-sm'>Lorem ipsum dolor sit amet consectetur. Diam</p>
+    <div className='border-gray-100 flex flex-col gap-4 rounded-md border-2 border-opacity-10 backdrop-blur py-9 px-10 w-[15rem]'>
+      <Image src={props.imgSrc} alt="" width={150} height={150} />
+      <h2 className="text-xl text-white-1">{props.title}</h2>
+      <p className='text-sm'>{props.description}</p>
     </div>
   )
 }
@@ -11,10 +20,10 @@ export const ListPrestasi = () => {
   return (
     <section className='flex flex-wrap text-center justify-center gap-5 mx-10'>
       <h1 className="w-full font-semibold text-5xl text-white-1 mb-5">Prestasi</h1>
-      <Prestasi />
-      <Prestasi />
-      <Prestasi />
-      <Prestasi />
+      <Prestasi imgSrc="https://i.kym-cdn.com/photos/images/newsfeed/002/652/460/d70.jpg" title="Heading" description="bejir" />
+      <Prestasi imgSrc="https://i.kym-cdn.com/photos/images/newsfeed/002/652/460/d70.jpg" title="Heading" description="bejir" />
+      <Prestasi imgSrc="https://i.kym-cdn.com/photos/images/newsfeed/002/652/460/d70.jpg" title="Heading" description="bejir" />
+      <Prestasi imgSrc="https://i.kym-cdn.com/photos/images/newsfeed/002/652/460/d70.jpg" title="Heading" description="bejir" />
     </section>
   )
 }
